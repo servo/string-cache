1fa8d0317a327d43846139281520e9061cab151f
Jack Moffitt <jack@metajack.im>
2015-06-09 16:43:15 -0600

    Minor changes for new Rust.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@
 #![crate_type = "rlib"]
 
 #![feature(plugin, unsafe_no_drop_flag)]
-#![feature(core, collections, alloc, hash)]
+#![feature(core, alloc, hash)]
 #![deny(warnings)]
 #![cfg_attr(test, feature(test))]
 #![cfg_attr(bench, feature(rand))]
