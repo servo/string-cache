570fea040ab3c964add1b2c74916c4c49dd7610d
Simon Sapin <simon.sapin@exyr.org>
2016-10-25 20:01:31 +0200

    Remove the big static atom list. Fix #22
    It’s going into various atom types in html5ever and Servo.

--- build.rs
+++ build.rs
@@ -1,14 +1,13 @@
 extern crate string_cache_codegen;
 
-#[path = "src/static_atom_list.rs"]
-mod static_atom_list;
-
 use std::env;
 use std::path::Path;
 
 fn main() {
     string_cache_codegen::AtomType::new("atom::tests::TestAtom", "test_atom!")
-        .atoms(static_atom_list::ATOMS)
+        .atoms(&[
+            "a", "b", "address", "area", "body", "font-weight", "br", "html", "head", "id",
+        ])
         .write_to_file(&Path::new(&env::var("OUT_DIR").unwrap()).join("test_atom.rs"))
         .unwrap()
 }
