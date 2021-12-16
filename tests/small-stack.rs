7934bf9eb7784962d5af8f96258756fc480d4c22
Michael Howell <michael@notriddle.com>
2021-12-16 16:16:49 -0700

    Fix warning and typo in small stack regression test

--- tests/small-stack.rs
+++ tests/small-stack.rs
@@ -1,6 +1,6 @@
 // Regression test for https://github.com/servo/html5ever/issues/393
 //
-// Create a dynamic atom − causing initialization of the golbal hash map −
+// Create a dynamic atom − causing initialization of the global hash map −
 // in a thread that has a small stack.
 //
 // This is a separate test program rather than a `#[test] fn` among others
@@ -9,7 +9,7 @@ fn main() {
     std::thread::Builder::new()
         .stack_size(50_000)
         .spawn(|| {
-            string_cache::DefaultAtom::from("12345678");
+            let _atom = string_cache::DefaultAtom::from("12345678");
         })
         .unwrap()
         .join()
